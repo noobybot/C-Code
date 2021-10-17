777e61badceef524d5bfde214706b50131b4c4a8
The Silent & Lonely Hacker <58019822+HiralPadher@users.noreply.github.com>
2021-10-17 17:07:19 +0530

    Update 1_helloworld.c

--- 1_helloworld.c
+++ 1_helloworld.c
@@ -3,7 +3,53 @@ int main()          // main function
 {
     printf("Hello world !");
 }
+/*
+#include <stdio.h>  
 
+int main()  
+
+{  
+
+    int n,m;  
+
+    printf("Enter the number of rows");  
+
+    scanf("%d",&n);  
+
+    m=n;  
+
+   for(int i=1;i<=n;i++)  
+
+   {  
+
+       for(int j=1;j<=m-1;j++)  
+
+       {  
+
+           printf(" ");  
+
+       }  
+
+       for(int k=1;k<=2*i-1;k++)  
+
+       {  
+
+         printf("*");  
+
+       }  
+
+       m--;  
+
+     
+
+      printf("\n");  
+
+    }  
+
+    return 0;  
+
+}  
+*/
 /*
 compile code using:         gcc filename.c
 execute a executable using: ./a.exe (windows) OR ./a.out (linux)
